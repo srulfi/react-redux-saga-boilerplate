2722716ff766cdb3da1e031b4c1d961dba02fbca
srulfi <santiagorulfi@gmail.com>
2019-02-12 16:42:27 -0300

    move store creation to index file, leave App with router rendering

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -1,31 +1,27 @@
 import React, { Component } from 'react';
-import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/lib/integration/react';
-import { Router, Switch, Route } from 'react-router-dom';
+import { Router, Route, Switch } from 'react-router-dom';
 import createHistory from 'history/createBrowserHistory';
-import configureStore from '../../configureStore';
 import './App.css';
 
 import HomePage from '../HomePage/';
+import LoginPage from '../LoginPage/';
+import DashboardPage from '../DashboardPage/';
 import NotFoundPage from '../NotFoundPage/';
 
-const initialState = {};
 const history = createHistory();
-const { store, persistor } = configureStore(initialState);
 
 class App extends Component {
+
   render() {
     return (
-      <Provider store={store}>
-        <PersistGate loading={null} persistor={persistor}>
-          <Router history={history}>
-            <Switch>
-              <Route path="/" exact component={HomePage} />
-              <Route component={NotFoundPage} />
-            </Switch>
-          </Router>
-        </PersistGate>
-      </Provider>
+      <Router history={history}>
+        <Switch>
+          <Route path="/" exact component={HomePage} />
+          <Route path="/login" exact component={LoginPage} />
+          <Route path="/dashboard" exact component={DashboardPage} />
+          <Route component={NotFoundPage} />
+        </Switch>
+      </Router>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/lib/integration/react';
 import App from './containers/App/';
 import * as ServiceWorker from './services/ServiceWorker';
+import configureStore from './configureStore';
+import './index.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {};
+const { store, persistor } = configureStore(initialState);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
