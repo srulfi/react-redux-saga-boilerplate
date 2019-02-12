d297871055d34e68901c35897ad534c5cbbb2d10
srulfi <santiagorulfi@gmail.com>
2019-02-12 16:40:16 -0300

    connect homePage to redux, kick syncUser saga when mounting and handle redirect

--- src/containers/HomePage/index.js
+++ src/containers/HomePage/index.js
@@ -1,7 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { Link, Redirect } from 'react-router-dom';
+import PropTypes from 'prop-types';
 
-const HomePage = () => (
-  <h1>Home.</h1>
-);
+import { AuthActions } from '../../actions';
 
-export default HomePage;
+class HomePage extends Component {
+
+	componentDidMount() {
+		const { syncUser } = this.props;
+
+		syncUser();
+	}
+
+	render() {
+		const { loggedIn } = this.props;
+
+		if (loggedIn) {
+			return <Redirect to="/dashboard" />;
+		}
+
+		return (
+			<div>
+		  	<h1>Home Page</h1>
+		  	<Link to="/login">go to login</Link>
+		  </div>
+		);
+	}
+}
+
+HomePage.propTypes = {
+  loggedIn: PropTypes.bool.isRequired,
+};
+
+const mapStateToProps = state => ({
+  loggedIn: state.auth.loggedIn,
+});
+
+const mapDispatchToProps = dispatch => ({
+  syncUser: () => dispatch(AuthActions.syncUser()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(HomePage);
